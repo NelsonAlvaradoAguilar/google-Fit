82e996557cf716c647bc231caae67fc52425b6f7
NelsonAlvaradoAguilar <nelsonalvaradoaguilar@gmail.com>
2024-04-20 18:21:30 -0400

    Airoll section modal functional

--- google-fit/src/components/QuestionaryModal/QuestionaryModal.js
+++ google-fit/src/components/QuestionaryModal/QuestionaryModal.js
@@ -2,15 +2,16 @@ import React from "react";
 import "./QuestionaryModal.scss";
 import { useNavigate } from "react-router-dom";
 
-function QuestionaryModal({ setModalIsOpen, isOpen, answer, closeModal }) {
+function QuestionaryModal({ isOpen, answer, closeModal }) {
   if (isOpen) return null;
   return (
     <section className="modal">
       <div className="modal__exit" cursor={"pointer"}>
-        <p onClick={closeModal} className="modal__confirmation">
-          {answer}
-        </p>
+        {" "}
       </div>
+      <p onClick={closeModal} className="modal__confirmation">
+        {answer}
+      </p>
     </section>
   );
 }
