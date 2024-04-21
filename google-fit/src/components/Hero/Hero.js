7ea85159befc562ac853654194388bdae6e41f12
Krista Chung <kchung187@gmail.com>
2024-04-21 03:47:00 -0600

    Remove text upon expanded image

--- google-fit/src/components/Hero/Hero.js
+++ google-fit/src/components/Hero/Hero.js
@@ -6,7 +6,11 @@ import heroImageRight from "../../assets/images/hero-right-enlarged.png";
 
 function Hero() {
   const [enlargedImage, setEnlargedImage] = useState(null);
-
+  const [expandedText, setExpandedText] = useState({
+    left: "Left Text",
+    middle: "middle Text",
+    right: "right Text",
+  });
   const handleClick = (image) => {
     if (enlargedImage === image) {
       setEnlargedImage(null);
@@ -16,13 +20,13 @@ function Hero() {
   };
   return (
     <div className="hero">
-      <p className="hero-text__left">
+      <p className={`hero-text__left${enlargedImage ? "--hide" : ""}`}>
         Personalized fitness recommendations await!
       </p>
-      <p className="hero-text__middle">
+      <p className={`hero-text__middle${enlargedImage ? "--hide" : ""}`}>
         Fitness companion that offers stress management{" "}
       </p>
-      <p className="hero-text__right">
+      <p className={`hero-text__right${enlargedImage ? "--hide" : ""}`}>
         Get personalized nutrition tips instantly
       </p>
       <div className="hero-img-container">
@@ -35,14 +39,20 @@ function Hero() {
           onClick={() => handleClick("left")}
         ></img>
         <img
-          className="hero-image__middle"
+          className={`hero-image__middle${
+            enlargedImage === "middle" ? "--expanded" : ""
+          }`}
           src={heroImageMiddle}
           alt="Three girls running on a beach"
+          onClick={() => handleClick("middle")}
         ></img>
         <img
-          className="hero-image__right"
+          className={`hero-image__right${
+            enlargedImage === "right" ? "--expanded" : ""
+          }`}
           src={heroImageRight}
           alt="Three girls running on a beach"
+          onClick={() => handleClick("right")}
         ></img>
       </div>
     </div>
